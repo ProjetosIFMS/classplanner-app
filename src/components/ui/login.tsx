9474e627b2186d6dad2e7d607eec00026f4a8cde
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-01-12 16:06:13 -0400

    feat: added simple login layout

--- src/components/ui/login.tsx
+++ src/components/ui/login.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import {Button} from "./button"
+import {Separator} from "./separator"
+import Link from "next/link";
+import Image from "next/image";
+
+
 
 export const Login = () => {
+   
 
     return (
-        <Button> Acesse com o email institucional</Button>
+        <section className="flex flex-col">
+            <h2 className="text-center mb-4 font-semibold">Faça seu login</h2>
+        
+            <p className="text-sm text-muted-foreground">Continue com seu e-mail institucional</p>
+            <Separator className="my-3"/>
+            <Button variant="outline">
+                <Image aria-hidden src="/gmail.svg" alt="Gmail icon" rel="icon" width={13} height={13}/>
+                <Link href="http://localhost:3000/google">Login com Gmail</Link>
+            </Button>
+
+
+        </section>
+       
+        
     )
 }
