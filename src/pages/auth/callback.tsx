0b7114ebbcbf35aecab037a77896bb278b3c25a8
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-01-14 18:47:42 -0400

    feat: simple auth validation implemented

--- src/pages/auth/callback.tsx
+++ src/pages/auth/callback.tsx
@@ -1,41 +1,29 @@
+"use server"
 import { useRouter } from "next/router"
 import { useEffect } from "react";
 
-export const GoogleAuthCallback = async () => {
+export default function GoogleAuthCallback() {
     const router = useRouter();
 
     async function storeToken(data: string){
         localStorage.setItem("jwtToken", data)
-        console.log(data)
     }
 
-    useEffect( () => {
-       const handleCallback = async () => {
-        try{
-            const response = await fetch("http://localhost:3000/google/redirect", {
-                credentials: "include",
-                headers:{
-                    "Accept": "application/json",
-                }
-            })
-
-            if (!response.ok){
-                throw new Error("Erro ao autenticar com o Google")
-            }
-
-            const data = await response.json();
-           
-            storeToken(data)
-            router.push('/dashboard')
-        } catch(error){
-            router.push("/login")
-        }}
-
-        if (router.query.code){
-            handleCallback();
+    useEffect(() => {
+      const handleToken = async () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get('access_token');
+  
+        if (!token) {
+          return router.push('/');
         }
-
-    }, [router])
+  
+        storeToken(token);
+        router.push('/dashboard');
+      };
+  
+      handleToken();
+    }, [router]);
 
     
 
