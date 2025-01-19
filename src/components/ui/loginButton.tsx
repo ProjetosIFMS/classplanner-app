465dca5ddcdeaf3c41565542a7be7728756b97e8
VitorSLima <dev.vitorlima@gmail.com>
2025-01-19 01:17:43 -0400

    fix: update image source in LoginButton component

--- src/components/ui/loginButton.tsx
+++ src/components/ui/loginButton.tsx
@@ -1,33 +1,33 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Button } from "./button";
-import { Separator } from "./separator";
-import Image from "next/image";
-import { signIn } from "next-auth/react";
+import React from 'react';
+import { Button } from './button';
+import { Separator } from './separator';
+import Image from 'next/image';
+import { signIn } from 'next-auth/react';
 
 export const LoginButton = () => {
   const handleLogin = async () => {
-    await signIn("google", {
+    await signIn('google', {
       redirect: true,
-      callbackUrl: "/dashboard",
+      callbackUrl: '/dashboard',
     });
   };
 
   return (
-    <section className="flex flex-col">
-      <h2 className="text-center mb-4 font-semibold">Faça seu login</h2>
+    <section className='flex flex-col'>
+      <h2 className='text-center mb-4 font-semibold'>Faça seu login</h2>
 
-      <p className="text-sm text-muted-foreground">
+      <p className='text-sm text-muted-foreground'>
         Continue com seu e-mail institucional
       </p>
-      <Separator className="my-3" />
-      <Button variant="outline" onClick={handleLogin}>
+      <Separator className='my-3' />
+      <Button variant='outline' onClick={handleLogin}>
         <Image
           aria-hidden
-          src="/gmail.svg"
-          alt="Gmail icon"
-          rel="icon"
+          src='/gmail.png'
+          alt='Gmail icon'
+          rel='icon'
           width={13}
           height={13}
         />
