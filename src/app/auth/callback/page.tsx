8b0f23a04eee9862964ad09219457240c3b6022c
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-01-20 14:52:43 -0400

    refactor: changed callback page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,33 +1,7 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function CallbackPage() {
-  const router = useRouter();
-
-  async function storeToken(data: string) {
-    localStorage.setItem("jwtToken", data);
-  }
-
-  useEffect(() => {
-    const handleToken = async () => {
-      const urlParams = new URLSearchParams(window.location.search);
-      const token = urlParams.get("access_token");
-
-      if (!token) {
-        return router.push("/");
-      }
-
-      storeToken(token);
-      router.push("/dashboard-professor");
-    };
-
-    handleToken();
-  }, [router]);
-
   return (
-    <h2 className="flex items-center text-muted-foreground text-xl">
-      Carregando...
-    </h2>
+    <div>
+      <h1>Carregando...</h1>
+    </div>
   );
 }
