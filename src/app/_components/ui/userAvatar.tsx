72b7120095ab3fa6a66fd0e6cf2ef43f49ec0272
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-01-21 11:02:48 -0400

    feat: add user data to header

--- src/app/_components/ui/userAvatar.tsx
+++ src/app/_components/ui/userAvatar.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "./avatar";
+import { User } from "@/types/user";
 
-export const UserAvatar = () => {
+export const UserAvatar = ({ email, firstName, lastName, picture }: User) => {
   return (
     <div className="flex flex-row items-center p-1 rounded-full">
       <button>
         <Avatar className="on-hover:bg-black">
-          <AvatarImage src="/default_avatar.png" alt="Default avatar" />
+          <AvatarImage
+            src={picture ? picture : "/default_avatar.png"}
+            alt="Default avatar"
+          />
           <AvatarFallback>avatar</AvatarFallback>
         </Avatar>
       </button>
       <div className="flex flex-col ml-2 items-start">
-        <p className="text-sm">Nome do usuário</p>
-        <p className="text-sm text-muted-foreground">email@usuario.com</p>
+        <p className="text-sm">
+          {firstName ? firstName + " " + lastName : "Usuário"}
+        </p>
+        <p className="text-sm text-muted-foreground">{email}</p>
       </div>
     </div>
   );
