8121fb351bdda500ecec668a81d1f215f2522f2d
VitorSLima <dev.vitorlima@gmail.com>
2025-01-27 09:46:25 -0400

    feat: replace user avatar button with logout button in header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,32 +1,28 @@
-import React from "react";
-import Image from "next/image";
-import { Card } from "./ui/card";
-import { ToggleStyle } from "./ui/toggleStyle";
-import { UserAvatar } from "./ui/userAvatar";
-import { getUserData } from "../_actions/getUserData";
-import { deleteUserToken } from "../_actions/deleteUserToken";
+import React from 'react';
+import Image from 'next/image';
+import { Card } from './ui/card';
+import { ToggleStyle } from './ui/toggleStyle';
+import { getUserData } from '../_actions/getUserData';
+import LogoutButton from './ui/LogoutButton';
 
 export const Header = async () => {
   const data = await getUserData();
-  
-  
+
   return (
-    <header className="w-full bg-white">
-      <Card className="flex justify-between items-center px-3 py-3">
+    <header className='w-full bg-white'>
+      <Card className='flex justify-between items-center px-3 py-3'>
         <Image
           aria-hidden
-          src="/logo.png"
-          alt="Class Planner icon"
-          rel="icon"
-          className="object-contain"
+          src='/logo.png'
+          alt='Class Planner icon'
+          rel='icon'
+          className='object-contain'
           width={60}
           height={60}
         />
-        <div className="flex items-center">
+        <div className='flex items-center'>
           <ToggleStyle />
-          <button onClick={deleteUserToken} >
-            <UserAvatar {...data} />
-          </button>
+          <LogoutButton data={data} />
         </div>
       </Card>
     </header>
