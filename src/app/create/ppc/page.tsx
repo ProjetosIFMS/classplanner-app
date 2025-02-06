22083c2df99026125527bfa4841666483b5e70dd
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-02-06 14:18:38 -0400

    feat: created pedagogical project form

---
+++ src/app/create/ppc/page.tsx
@@ -0,0 +1,16 @@
+"use server";
+import { Header } from "@/app/_components/header";
+import { PPCForm } from "./ppc-form";
+
+const CreatePPC = async () => {
+  return (
+    <section className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex items-center justify-center">
+        <PPCForm />
+      </main>
+    </section>
+  );
+};
+
+export default CreatePPC;
---
+++ src/types/validation/forms.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const ppcSchema = z.object({
+  PPCYear: z.coerce
+    .number({
+      message: "O ano de criação é necessário",
+      invalid_type_error: "Campo deve ser composto de números",
+    })
+    .min(2000, { message: "O ano de criação deve ser maior que o de 2000" }),
+  semesterQuantity: z.coerce
+    .number({ message: "A quantidade de semestres é necessária" })
+    .min(1, "A quantidade de semestres não pode ser menor que 1")
+    .max(12, "A quantidade de semestres deve ser menor que 12"),
+  hasTCC: z.coerce.number().min(0).max(1).default(1),
+  extensionCourses: z.coerce
+    .number()
+    .min(0, "Número de aulas de extensão não pode ser menor que 0")
+    .max(5, "Número de aulas de extensão não pode ser maior que 5")
+    .optional()
+    .or(z.literal(0)),
+  workload: z.coerce
+    .number({
+      message: "A carga horária é necessária",
+      invalid_type_error: "Carga horária deve ser um número",
+    })
+    .min(1000, "A carga horária não pode ser menor que 1000")
+    .max(4000, "A carga horária não pode ser maior que 4000"),
+  additionalHours: z.coerce.number().optional().or(z.literal(0)),
+  internshipHours: z.coerce.number().optional().or(z.literal(0)),
+  description: z
+    .string()
+    .min(10, "A descrição deve conter mais que 10 caracteres")
+    .optional()
+    .or(z.literal("")),
+});
+
+export type PPCSchema = z.infer<typeof ppcSchema>;
