acc0622d91ff5e7d6945261dbcc3fa2309293bb9
Rafael Ortiz Nunes <83882306+farxC@users.noreply.github.com>
2025-02-07 11:18:08 -0400

    refactor: removed comments

--- src/app/create/ppc/actions.ts
+++ src/app/create/ppc/actions.ts
@@ -2,17 +2,5 @@ import { PPCSchema } from "@/types/validation/forms";
 import api from "@/utils/axios-instance";
 
 export async function submitPPC(formData: PPCSchema) {
-  // const validateFields = ppcSchema.parse({
-  //     PPCYear: formData.PPCYear,
-  //     semesterQuantity: formData.semesterQuantity,
-  //     hasTCC: formData.hasTCC,
-  //     additionalHours: formData.additionalHours,
-  //     extensionCourses: formData.extensionCourses,
-  //     workload: formData.workload,
-  //     internshipHours: formData.internshipHours,
-  //     description: formData.description
-  // })
-
-  // Need API refactor
   api.post("/pedagogical-project", formData);
 }
