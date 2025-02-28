ea5a811c63be2b36311a2e65b9c40f272d437ba1
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-02-28 10:52:45 -0400

    refactor: changed server components to handle the auth context properly

--- src/app/_actions/deleteUserToken.ts
+++ src/app/_actions/deleteUserToken.ts
@@ -1,11 +1,10 @@
-"use server"
+"use server";
 
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-
 export const deleteUserToken = async () => {
-    const cookieStore = await cookies();
-    cookieStore.delete("access_token")
-    redirect('/')
-}
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+  redirect("/auth/login");
+};
