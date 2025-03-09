668d829979ab22416cf449a3c564f6eab15cbf2d
Rafael Ortiz Nunes <83882306+farxC@users.noreply.github.com>
2025-03-09 08:19:58 -0400

    Update modality_form.ts

--- src/types/validation/modality_form.ts
+++ src/types/validation/modality_form.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const modalitySchema = z.object({
-    name: z.string().min(1, "O nome da deve conter mais que 1 caracter"),
+    name: z.string().min(1, "O nome da modalidade deve conter mais que 1 caracter"),
 
 });
 
