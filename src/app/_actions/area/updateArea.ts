ceae6386b0b9a1a4307aaaaf4702bd9b5151bafc
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-03-20 12:56:22 -0400

    feat: add list area

---
+++ src/app/_actions/area/updateArea.ts
@@ -0,0 +1,20 @@
+import { AreaValues } from "@/types/validation/area_form";
+import api from "@/utils/axios-instance";
+
+export const updateArea = async (
+  session: string | undefined,
+  area_id: string,
+  formData: AreaValues,
+) => {
+  try {
+    const res = api.patch(`/area/${area_id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${session}`,
+      },
+    });
+
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
