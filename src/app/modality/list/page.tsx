a1e52e9029e13c0fccb4d6ab3265f31f860c8769
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-04-24 01:49:30 -0300

    refactor: update user role handling in DataTable and ListModalities components

--- src/app/modality/list/page.tsx
+++ src/app/modality/list/page.tsx
@@ -10,9 +10,10 @@ import { Modality } from "@/types/modality";
 import { createColumns } from "./components/columns";
 import { DataTable } from "./components/data-table";
 import { deleteModality } from "@/app/_actions/modality/deleteModality";
+import { Role } from "@/types/user";
 
 const ListModalities = () => {
-  const { session } = useAuth();
+  const { session, user } = useAuth();
   const [data, setData] = useState<Modality[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -33,12 +34,12 @@ const ListModalities = () => {
 
   const handleDeleteModality = async (
     session: string | undefined,
-    modality_id: string,
+    modality_id: string
   ) => {
     const success = await deleteModality(session, modality_id);
     if (success) {
       setData((prevData) =>
-        prevData.filter((modality) => modality.id !== modality_id),
+        prevData.filter((modality) => modality.id !== modality_id)
       );
     }
     return success;
@@ -51,12 +52,14 @@ const ListModalities = () => {
       <div className="flex-1 p-6 max-w-7xl mx-auto w-full">
         <div className="flex items-center justify-between mb-6">
           <h1 className="text-2xl font-bold">Modalidades</h1>
-          <Button asChild>
-            <Link href="/modality/create">
-              <Plus className="mr-2 h-4 w-4" />
-              Nova Modalidade
-            </Link>
-          </Button>
+          {user?.role === Role.COORDINATOR && (
+            <Button asChild>
+              <Link href="/modality/create">
+                <Plus className="mr-2 h-4 w-4" />
+                Nova Modalidade
+              </Link>
+            </Button>
+          )}
         </div>
         <div className="bg-white dark:bg-gray-950 rounded-lg shadow p-6">
           <DataTable
