1e235afbaf1af7bb8cd7ab28491da54eb2063ec5
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-04-29 12:19:50 -0400

    feat: increased character limit at area_form

--- src/types/validation/area_form.ts
+++ src/types/validation/area_form.ts
@@ -4,7 +4,7 @@ export const areaSchema = z.object({
   name: z
     .string({ message: "Necessário inserir o nome da área" })
     .min(1, "O nome da área deve conter mais que 1 caracter")
-    .max(40, "O nome da área deve conter menos que 40 caracteres"),
+    .max(80, "O nome da área deve conter menos que 80 caracteres"),
 });
 
 export type AreaValues = z.infer<typeof areaSchema>;
